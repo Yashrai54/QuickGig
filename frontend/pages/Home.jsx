7857821f0cf045413abb8c12b4514c2ce6f9338f
Yashrai54 <starrai843@gmail.com>
2026-09-12 18:03:19 +0530

    motion div changes

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -7,68 +7,72 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-   const [nearbyProjects,setNearbyProjects] = useState([])
-   const [message,setMsg] = useState("")
-   const [search, setSearch] = useState("")
-   const navigate = useNavigate()
-   function getLocation(){
-    return new Promise((res,rej)=>{
-      navigator.geolocation.getCurrentPosition((p)=>res({
-        lat:p.coords.latitude,
-        lng:p.coords.longitude
+  const [nearbyProjects, setNearbyProjects] = useState([])
+  const [message, setMsg] = useState("")
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+  function getLocation() {
+    return new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition((p) => res({
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
       }
-      ),rej)
+      ), rej)
     })
-   }
-   async function getLocationAndSearch() {
-  if (!search.trim()) return
-
-  try {
-    const res = await axios.get(
-      "https://quickgig-jous.onrender.com/api/project/search",
-      {
-        params: {
-          query: search
-        },
-        withCredentials: true
-      }
-    )
-
-    navigate("/near", {
-      state: {
-        projects: res.data.foundProjects,
-        message: res.data.message
-      }
-    })
-  } catch (error) {
-    console.error(error)
-    setMsg("Something Broke")
   }
-}
-   async function handleNearbyProjects(){
-      const {lat,lng} = await getLocation()
+  async function getLocationAndSearch() {
+    if (!search.trim()) return
 
-      try{
-        console.log(lat)
-        console.log(lng)
-          const res = await axios.get(`https://quickgig-jous.onrender.com/api/project/near/${lat}/${lng}`,{withCredentials:true})
-          console.log(res)
-          setNearbyProjects(res.data.foundProjects)
-          setMsg(res.data.message)
-          navigate("/near",{state:{projects:res.data.foundProjects,message:res.data.message}})
-      }
-      catch(error){
-          setMsg("Something Broke")
-      }
-   }
+    try {
+      const res = await axios.get(
+        "https://quickgig-jous.onrender.com/api/project/search",
+        {
+          params: {
+            query: search
+          },
+          withCredentials: true
+        }
+      )
+
+      navigate("/near", {
+        state: {
+          projects: res.data.foundProjects,
+          message: res.data.message
+        }
+      })
+    } catch (error) {
+      console.error(error)
+      setMsg("Something Broke")
+    }
+  }
+  async function handleNearbyProjects() {
+    const { lat, lng } = await getLocation()
+
+    try {
+      console.log(lat)
+      console.log(lng)
+      const res = await axios.get(`https://quickgig-jous.onrender.com/api/project/near/${lat}/${lng}`, { withCredentials: true })
+      console.log(res)
+      setNearbyProjects(res.data.foundProjects)
+      setMsg(res.data.message)
+      navigate("/near", { state: { projects: res.data.foundProjects, message: res.data.message } })
+    }
+    catch (error) {
+      setMsg("Something Broke")
+    }
+  }
+  const isMobile = window.innerWidth < 1024
+
   return (
     <div className='min-h-screen bg-[#F2FAFA]'>
       <Navbar />
 
       <main className='m-5 flex flex-col lg:flex-row justify-center items-center gap-[20px] lg:gap-[40px] px-4'>
-        <motion.div initial={{ opacity: 0, x: 0 }} animate={{ opacity: 1, x: 30 }}
+        <motion.div
+          initial={{ opacity: 0, x: 0 }}
+          animate={{ opacity: 1, x: isMobile ? 0 : 30 }}
           transition={{ duration: 0.8, ease: "easeIn" }}
-          className='w-full max-w-[700px] mt-10'> 
+        >
           <h1 className='text-2xl md:text-3xl lg:text-4xl font-bold text-black text-center lg:text-left'>
             Are you finding local clients?
           </h1>
@@ -77,24 +81,25 @@ const Home = () => {
             Browse local gigs, connect with nearby clients, and get paid for what you do best
             — all in your area.
           </p>
-        <div className='flex flex-col sm:flex-row items-center justify-center mt-10 gap-3 sm:gap-0'>
-          <button className='bg-sky-500 text-white font-semibold px-6 sm:px-10 py-3 sm:py-4
+          <div className='flex flex-col sm:flex-row items-center justify-center mt-10 gap-3 sm:gap-0'>
+            <button className='bg-sky-500 text-white font-semibold px-6 sm:px-10 py-3 sm:py-4
              rounded-md cursor-pointer w-full sm:w-auto
-            ' onClick={()=>handleNearbyProjects()}>
+            ' onClick={() => handleNearbyProjects()}>
               Check for Projects
             </button>
-          <input type="search" name="job-search" id="job-search" value={search} onChange={(e)=>setSearch(e.target.value)}
-            placeholder='Search freelance work globally' className='sm:ml-5 px-4 py-3 sm:py-4 border-2 border-sky-500 rounded-md outline-none
+            <input type="search" name="job-search" id="job-search" value={search} onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search freelance work globally' className='sm:ml-5 px-4 py-3 sm:py-4 border-2 border-sky-500 rounded-md outline-none
              transition-all duration-200 focus:border-sky-500 focus:ring-2 focus:ring-sky-200 w-full sm:w-auto
              '  />
-          <span className="items-center sm:ml-3">
-            <CiSearch size={30} className='cursor-pointer' onClick={()=>getLocationAndSearch()} />
-          </span>
-        </div>
+            <span className="items-center sm:ml-3">
+              <CiSearch size={30} className='cursor-pointer' onClick={() => getLocationAndSearch()} />
+            </span>
+          </div>
         </motion.div>
         <div className='w-full max-w-[500px] h-auto mt-10 lg:mt-0'>
           <img src={HomePageBg} alt="" className='w-full h-auto' />
         </div>
+        
       </main>
     </div>
   )
